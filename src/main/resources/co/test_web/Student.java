34930fe378e522895cf318b184cf6a6a5c2e68b4
jiangyd3 <jiangyd3@asiainfo.com>
2019-06-18 17:43:19 +0800

    合并2

--- src/main/resources/co/test_web/Student.java
+++ src/main/resources/co/test_web/Student.java
@@ -10,7 +10,6 @@ public class te {
         try
         {
             int count=input.nextInt();
-            System.out.println("请输入总成绩：");
             int score=input.nextInt();
             int avg=score/count;    //获取平均分
             System.out.println("本次考试的平均分为："+avg);
