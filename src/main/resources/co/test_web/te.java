ff56debde70ba3091a43e42ab4feb2eeea8d703f
jiangyd3 <jiangyd3@asiainfo.com>
2019-06-18 16:23:48 +0800

    重置

--- src/main/resources/co/test_web/te.java
+++ src/main/resources/co/test_web/te.java
@@ -32,19 +32,52 @@ public class te {
     }
 	
 	public int getInt() {
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
 		return 1;
 	}
 	
 	public int getInt1() {
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		String a = "sssssssssss";
+		
 		return 1;
 	}
 	
 	public int getInt3() {
 		return 3;
-	}
-	
-	public int getA() {
-		return 2;
 	}
 	
 	public int getInt2() {
